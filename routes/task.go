79899108860638ebbde18ca611a755cdfd040a1e
yk-jp <k82.y12@gmail.com>
2022-11-27 10:33:03 -0800

    add get reqest to get specific data

--- routes/task.go
+++ routes/task.go
@@ -5,14 +5,38 @@ import (
 	"github.com/yk-jp/todo-backend/database"
 	"github.com/yk-jp/todo-backend/database/schema"
 	"github.com/yk-jp/todo-backend/models"
+	"github.com/yk-jp/todo-backend/utils"
 )
 
 func GetTasks(c *fiber.Ctx) error {
 	responseData := []models.Task{}
 
+	// response := database.Db.Db.Model(&models.Task{}).
+	// 	Select("tasks.id, title, status_refer, statuses.name as status").
+	// 	Joins("left join statuses on statuses.id = tasks.status_refer").
+	// 	Scan(&responseData)
+
+	response := utils.FindAllTasks(&responseData)
+
+	if response.Error != nil {
+		return fiber.ErrInternalServerError
+	}
+
+	return c.Status(200).JSON(responseData)
+}
+
+func GetTask(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+
+	if err != nil {
+		return fiber.ErrBadRequest
+	}
+
+	var responseData models.Task
 	response := database.Db.Db.Model(&models.Task{}).
 		Select("tasks.id, title, status_refer, statuses.name as status").
 		Joins("left join statuses on statuses.id = tasks.status_refer").
+		Where("tasks.id = ?", id).
 		Scan(&responseData)
 
 	if response.Error != nil {
